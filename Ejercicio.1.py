5086015dc115fe013ea252f0a6a140d23ed605dd
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-11-06 22:00:17 +0100

    2nd commit

--- Ejercicio.1.py
+++ Ejercicio.1.py
@@ -2,3 +2,24 @@ print ("\n\n")
 class Alumno:
   def __init__(self, nombre, nota):
     self.nombre= nombre
+    self.nota= nota
+    print("El alumno se ha creado con éxito\n")
+    print("Nombre: ", self.nombre)
+    print("Nota: ", self.nota)
+
+
+  def califiacion(self):
+    if self.nota>=5:
+       print(self.nombre, "ha aprobado")
+    else:
+      print(self.nombre, "ha suspendido")
+
+alumno1=Alumno("Jaime", 5)
+alumno1.califiacion()
+
+print("\n\n")
+
+alumno2=Alumno("Alberto", 3)
+alumno2.califiacion()
+
+print("\n\n")
